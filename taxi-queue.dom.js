55edb13dcc7b04aa90641a3340c8ad5ae73a0c17
ThamsanqaMpontshane <thamsanqalm01@gmail.com>
2022-05-30 14:24:46 +0200

    **First Commit**

--- taxi-queue.dom.js
+++ taxi-queue.dom.js
@@ -2,10 +2,45 @@
 
 
 // DOM element references
-
+//Passengers
+var passengerCount = document.getElementsByClassName('passenger_queue_count')[0];
+var joinqueue = document.getElementsByClassName('join_queue')[0];
+var leavequeue = document.getElementsByClassName('leave_queue')[0];
+//Taxi
+var taxiCount = document.getElementsByClassName('taxi_queue_count')[0];
+var taxiJoin = document.getElementsByClassName('join_taxi_queue')[0];
+//Taxi Depart
+var taxiDepart = document.getElementsByClassName('depart')[0];
 // create Factory Function instance
 
 const taxiQueue = TaxiQueue();
 
 // DOM events
+joinqueue.addEventListener('click', function() {
+    taxiQueue.joinQueue();
+    passengerCount.innerHTML = taxiQueue.queueLength();
+}
+);
+
+
+leavequeue.addEventListener('click', function() {
+    taxiQueue.leaveQueue();
+    passengerCount.innerHTML = taxiQueue.queueLength();
+}
+);
+
+
+taxiJoin.addEventListener('click', function() {
+    taxiQueue.joinTaxiQueue();
+    taxiCount.innerHTML = taxiQueue.taxiQueueLength();
+}
+);
+
+
+taxiDepart.addEventListener('click', function() {
+    taxiQueue.taxiDepart();
+    taxiCount.innerHTML = taxiQueue.taxiQueueLength();
+}
+);
+
 
--- taxi-queue.js
+++ taxi-queue.js
@@ -1,28 +1,40 @@
 function TaxiQueue() {
-
+	var passengerQueue = [];
+	var taxiQueue = [];
+	var passengerCount = 0;
+	var taxiCount = 0;
+	// var taxiLeave = 0;
+	// var taxiJoin = 0;
 
 	function joinQueue() {
-
+		passengerCount++;
+		passengerQueue.push(passengerCount);
+		// passengerCount.innerHTML = passengerQueue.length;
 	}
 
 	function leaveQueue() {
+		passengerCount--;
+		passengerQueue.shift();
+	}
 
+    function queueLength() {
+		return passengerQueue.length;
 	}
 
 	function joinTaxiQueue() {
-
-	}
-
-	function queueLength() {
-
+		taxiCount++;
+		taxiQueue.push(taxiCount);
 	}
 
 	function taxiQueueLength() {
-
+		return taxiQueue.length;
 	}
 
-	function taxiDepart(){
-
+	function taxiDepart() {
+		if (passengerQueue.length >= 12 && taxiQueue.length >= 1) {
+			taxiQueue.shift();
+			passengerQueue.splice(0, 12);
+		}
 	}
 
 	return {
